c30b1f2aee64400a42b6a31d79b7f87b356fb0cb
labeneko <labecat@gmail.com>
2023-01-24 17:20:40 +0900

    PDF変換ツールを作成

---
+++ run.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const assert = require('assert');
+const puppeteer = require('puppeteer');
+const { basename } = require('path');
+const { exit } = require('process');
+(async() => {
+  const baseUrl = process.argv[2];
+  if (!baseUrl) {
+    console.log('node run.js http://localhost:2525 のようにURLを指定してください')
+    exit(1);
+  }
+  let args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--window-size=800,450'
+  ];
+  const browser = await puppeteer.launch({
+        headless: true,
+        args: args,
+        defaultViewport: {
+            width: 800,
+            height: 450
+          }
+  });
+  const page = await browser.newPage();
+
+  // PDF出力対象ページ
+  await page.goto(baseUrl);
+  const urls = await page.evaluate((baseUrl) => {
+    const list = Array.from(document.querySelectorAll('a')).filter(data => data.getAttribute('href'));
+    return list.map(data => {
+      return new URL(data.getAttribute('href'), baseUrl).toString()
+    });
+  }, baseUrl);
+
+  console.log('PDF変換を開始します')
+  const dateStr = new Date()
+            .toISOString()
+            .replace(/[^0-9]/g, '')
+            .slice(0, -5);
+  const dirpath = 'out/' + dateStr;
+  fs.mkdirSync(dirpath, { recursive: true });
+  for (let i = 0; i < urls.length; i++) {
+    const url = urls[i];
+    await page.goto(url);
+    const width = await page.evaluate(() => document.documentElement.offsetWidth);
+    const height = await page.evaluate(() => document.documentElement.offsetHeight);
+    // PDF作成処理
+    await page.pdf({
+        path: dirpath + '/' + basename(url).split('.')[0] + '.pdf',
+        printBackground: true,
+        margin: { top: '37px', right: '37px', bottom: '37px', left: '37px' },
+        width: 793,
+        height: height + 200,
+    });
+    process.stdout.write(`\r[${i+1}/${urls.length}] PDF変換中…`)
+  };
+  process.stdout.write("\n");
+  console.log('PDF変換が完了しました')
+  browser.close();
+})();
